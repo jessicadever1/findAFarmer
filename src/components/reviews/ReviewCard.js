2f9eed244d10477a1506e6d3e58414d2e2aeb109
jessicadever1 <jessicadever1@gmail.com>
2021-02-01 14:23:07 -0600

    added Review Card

--- src/components/reviews/ReviewCard.js
+++ src/components/reviews/ReviewCard.js
@@ -1,4 +1,20 @@
 import React from "react"
 import "./Review.css"
+import user from "../images/user.png"
 
-export const ReviewCard = ({})
+export const ReviewCard = ({ review }) => {
+
+    const userPic = user
+
+    return (
+        <article className="review">
+            <h3 className="review__name">{review.name}</h3>
+            <section className="review__userInfo">
+                <img src={userPic} alt="user default pic"></img>
+                <div className="review__username">{review.user.username}</div>
+                <div className="review__date">{review.date}</div>
+            </section>
+            <p className="review__reviewText">{review.reviewText}</p>
+        </article>
+    )
+}
