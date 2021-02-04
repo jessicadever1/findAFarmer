54cfba239ede4b347e169fc9300765d51a627cb3
jessicadever1 <jessicadever1@gmail.com>
2021-02-04 15:38:01 -0600

    completed filtration by including a see all farms btn

--- src/components/farm/AllFarmsBtn.js
+++ src/components/farm/AllFarmsBtn.js
@@ -1,15 +1,21 @@
-import React from "react"
 import "./Farm.css"
 import { Link } from "react-router-dom"
-
+import { FarmContext } from "./FarmProvider"
+import React, { useContext, useEffect, useState } from "react"
 
 export const AllFarmsBtn = () => {
 
+    const { getFarms } = useContext(FarmContext)
+    const [farms, setFarms] = useState([])
     
+    const handleSeeAllFarmsClick = () => {
+            getFarms()
+            .then(setFarms)
+    }
 
     return (
         <div className="allFarmsBtnDiv">
-            <Link to={'/farms'}><button className="allFarmsBtn">See All Farms</button></Link>
+            <button onClick={handleSeeAllFarmsClick} className="allFarmsBtn">See All Farms</button>
         </div>
     )
 }
