8ccfc1d8565d9b72b8cd9d29acd5d2d153ac85ba
jessicadever1 <jessicadever1@gmail.com>
2021-02-04 19:56:48 -0600

    see farms btn works

--- src/components/reviews/ReviewCard.js
+++ src/components/reviews/ReviewCard.js
@@ -1,13 +1,31 @@
-import React from "react"
+import React, { useContext } from "react"
 import "./Review.css"
 import userPicture from "../images/user.png"
 import trashIcon from "../images/trash.png"
+import { ReviewContext } from "./ReviewProvider"
+import { useHistory } from "react-router-dom"
 
 export const ReviewCard = ({ review, user, farm }) => {
 
+    const { deleteReview } = useContext(ReviewContext)
+
     const userPic = userPicture
-    console.log("review", review)
-    console.log("farmID", farm.id)
+
+    const currentUser = localStorage.getItem("find-a-farm_user")
+    const history = useHistory()
+
+    const handleClickDeleteReview = () => {
+        console.log("what is review id?", review.id)
+        console.log("what is currentUser?", currentUser)
+        console.log("what is review.userId?", review.userId)
+        if (currentUser === review.userId) {
+            deleteReview(review.id)
+            .then(() => {
+                history.push("/farms/detail/${farm.id}")
+            })
+        }
+    }
+
     return (
         <article className="review">
             <img className="reviewerProfilePic" src={userPic} alt="user default pic"></img>
@@ -18,7 +36,7 @@ export const ReviewCard = ({ review, user, farm }) => {
                 <div className="review__date">{review.date}</div>
                 <p className="review__reviewText">{review.reviewText}</p>
             </section>
-            <img className="" src="" alt="delete icon"></img>
+            <button onClick={handleClickDeleteReview} className="trashBtn"><img className="trashIcon" src={trashIcon} width="25" height="25" alt="delete icon"></img></button>
         </article>
     )
 }
