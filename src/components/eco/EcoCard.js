bc646a42b11c273fb726ed1601fd8d7e1651b821
jessicadever1 <jessicadever1@gmail.com>
2021-02-10 22:28:15 -0600

    added arrows

--- src/components/eco/EcoCard.js
+++ src/components/eco/EcoCard.js
@@ -1,15 +1,19 @@
 import React from "react"
 import "./Eco.css"
+import leftArrow from "../images/leftArrow.png"
+import rightArrow from "../images/rightArrow.png"
 
 export const EcoCard = ({ecoTip}) => {
     return (
         <>
             
+                <img src={leftArrow} height="100" width="100" alt="left arrow" className="leftArrow"></img>
                 <section className="EcoTipsCard">
                     <h3 className="ecoTitle">{ecoTip.title}</h3>
                     <article className="ecoText">{ecoTip.text}</article>
                 </section>
-        
+                <img src={rightArrow} height="100" width="100" alt="left arrow" className="rightArrow"></img>
+            
         </>
     )
 }
